aac044a16bda5a63dd2cdb46382351c5559a9815
unknown <yujin5681@sookmyung.ac.kr>
2026-09-17 14:17:06 -0700

    feat: Week 1 포켓몬 컴포넌트 구현

---
+++ src/components/PokemonList.tsx
@@ -0,0 +1,49 @@
+import PokemonCard from './PokemonCard.tsx'
+import pikachuImage from '../assets/pikachu.png'
+import charizardImage from '../assets/charizard.png'
+import bulbasaurImage from '../assets/Bulbasaur.png'
+import blastoiseImage from '../assets/Blastoise.png'
+
+const pokemons = [
+  {
+    number: '#0025',
+    name: '피카츄',
+    type: 'ELECTRIC',
+    image: pikachuImage,
+  },
+  {
+    number: '#0006',
+    name: '리자몽',
+    type: 'FIRE',
+    image: charizardImage,
+  },
+  {
+    number: '#0001',
+    name: '이상해씨',
+    type: 'GRASS',
+    image: bulbasaurImage,
+  },
+  {
+    number: '#0009',
+    name: '거북왕',
+    type: 'WATER',
+    image: blastoiseImage,
+  },
+]
+
+function PokemonList() {
+  return (
+    <div className="pokemon-list">
+      {pokemons.map((pokemon) => (
+        <PokemonCard
+          key={pokemon.number}
+          number={pokemon.number}
+          name={pokemon.name}
+          type={pokemon.type}
+          image={pokemon.image}
+        />
+      ))}
+    </div>
+  )
+}
+export default PokemonList
